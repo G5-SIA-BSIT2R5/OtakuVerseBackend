b392301e0e3f3c5db5fc0456ba70044e580c3f7a
Joana Mae Pailanan <pailananjoanamae1@gmail.com>
2025-05-07 22:42:21 +0800

    refactor: routes integration animequote

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimeQuoteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/anime-quote', [AnimeQuoteController::class, 'getRandomQuote']);
+
